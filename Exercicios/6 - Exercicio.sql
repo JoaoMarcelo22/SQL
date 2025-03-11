dccd203113b8f01e0a02ca95cc1a55923282d0fe
JoaoMarcelo22 <joao_marcelo.22@hotmail.com>
2025-03-10 22:59:45 -0300

    feat: Adicionando até o exercicio 3

--- Exercicios/6 - Exercicio.sql
+++ Exercicios/6 - Exercicio.sql
@@ -6,16 +6,73 @@ a) Faça um resumo da quantidade vendida (Sales Quantity) de acordo com o nome do
 b) Faça um agrupamento mostrando a quantidade total vendida (Sales Quantity) e quantidade total devolvida (Return Quantity) de acordo com o nome das lojas (StoreName).
 c) Faça um resumo do valor total vendido (Sales Amount) para cada mês (CalendarMonthLabel) e ano (CalendarYear).
 */
+--select top(10)
+--	ChannelName,
+--	sum(SalesQuantity) as 'Quantidade'
+--from
+--	FactSales
+--inner join DimChannel
+--	on FactSales.channelKey = DimChannel.ChannelKey
+--group by ChannelName
+--order by Quantidade desc -- aqui usamos o order by de acordo com o From inicial, nesse caso FactSales.
+
+--select top(10)
+--	StoreName,
+--	sum(SalesQuantity) as 'Quantidade Vendido',
+--	sum(ReturnQuantity) as 'Devolvido'
+--from
+--	FactSales
+--inner join DimStore
+--	on FactSales.StoreKey = DimStore.StoreKey
+--group by StoreName
+--order by StoreName
+
+select
+	CalendarYear as 'Ano',
+	CalendarMonthLabel as 'Mes',
+	sum(SalesAmount) as 'Faturamento total'
+from
+	FactSales
+inner join DimDate
+	on FactSales.DateKey = DimDate.Datekey
+group by CalendarYear,CalendarMonthLabel,CalendarMonth
+order by CalendarMonth asc
 
 /*
 2. Você precisa fazer uma análise de vendas por produtos. O objetivo final é descobrir o valor total vendido (SalesQuantity) por produto.
 a) Descubra qual é a cor de produto que mais é vendida (de acordo com SalesQuantity).
 b) Quantas cores tiveram uma quantidade vendida acima de 3.000.000.
 */
+select top(10)
+	ColorName as 'Cor',
+	sum(SalesQuantity) as 'qtd. Vendido'
+from
+	FactSales
+inner join DimProduct
+	on FactSales.ProductKey = DimProduct.ProductKey
+group by ColorName
+having sum(SalesQuantity) >= 3000000 -- Aqui o AS não funcionou
+order by [qtd. Vendido] desc
+
+
 
 /*
 3. Crie um agrupamento de quantidade vendida (SalesQuantity) por categoria do produto (ProductCategoryName). Obs: Você precisará fazer mais de 1 INNER JOIN, dado que a relação entre FactSales e DimProductCategory não é direta.
 */
+select
+	ProductCategoryName as 'Categoria produto',
+	Sum(SalesQuantity) as 'Qtd Total'
+from
+	factSales
+inner join DimProduct
+	on	factSales.ProductKey = DimProduct.ProductKey
+		inner join DimProductSubcategory
+			on	DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+				inner join DimProductCategory
+					on DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategoryKey
+group by ProductCategoryName
+order by [Qtd Total] desc
+
 
 /*
 4. a) Você deve fazer uma consulta à tabela FactOnlineSales e descobrir qual é o nome completo do cliente que mais realizou compras online (de acordo com a coluna SalesQuantity).
